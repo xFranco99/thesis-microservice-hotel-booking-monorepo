5ce851a0f3f22d3103a72978541f237e9191e732
giglio <francychicco@live.it>
2024-11-17 23:49:01 +0100

    add: CORS middleware

--- back-end/user-auth/src/main.py
+++ back-end/user-auth/src/main.py
@@ -1,10 +1,24 @@
 import uvicorn
 from fastapi import FastAPI
 from router.api import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8080)
