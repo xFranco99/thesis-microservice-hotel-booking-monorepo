109198f15ef1487a27c661d1eaca6b4dee4e9e30
giglio <francychicco@live.it>
2024-11-18 15:02:50 +0100

    refactor: removed unused imports

--- back-end/hotel_service/src/config/database.py
+++ back-end/hotel_service/src/config/database.py
@@ -1,7 +1,7 @@
-import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 from config.env_var import EnvVar
 
 DATABASE_URL = EnvVar.DATABASE_URL
