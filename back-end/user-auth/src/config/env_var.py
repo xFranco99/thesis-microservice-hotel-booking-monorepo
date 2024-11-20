6b96f16389d8825a163e9995ac01a7badd706f8e
giglio <francychicco@live.it>
2024-11-20 09:47:49 +0100

    fix: MAIL_SERVICE_BASE_URL restored

--- back-end/user-auth/src/config/env_var.py
+++ back-end/user-auth/src/config/env_var.py
@@ -8,4 +8,5 @@ class EnvVar:
     SECRET_KEY = os.environ['SECRET_KEY']  # openssl rand -hex 32
     ALGORITHM = os.environ['ALGORITHM']
 
+    MAIL_SERVICE_BASE_URL = os.environ['MAIL_SERVICE_BASE_URL']
 
