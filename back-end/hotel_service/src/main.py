0636eb64255687d5032becec9d8eec9caec29f68
giglio <francychicco@live.it>
2024-11-20 01:12:19 +0100

    add: CORS permiss

--- back-end/hotel_service/src/main.py
+++ back-end/hotel_service/src/main.py
@@ -1,8 +1,24 @@
 import uvicorn
 from fastapi import FastAPI
 from routers.api import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:8080",
+    "http://127.0.0.1:8081",
+    "http://127.0.0.1:8082",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 
